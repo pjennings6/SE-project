55f48b3b4c06acaa73b9a2cb0b54157899a58c09
Shormie Faruque <faruqueshormie@gmail.com>
2020-04-24 10:36:24 -0400

    added more member variables to constructor

--- lendingTree/src/lendingTree/loanTransaction.java
+++ lendingTree/src/lendingTree/loanTransaction.java
@@ -7,7 +7,47 @@ public class loanTransaction
 	String typeOfLoan; 
 	int amountDesired;
 	int downPayment;
+	dpackage sss5;
+
+public class loanTransaction 
+{
+	String name;
+	String cityName;
+	String stateName;
+	String zipCode; 
+	String phoneNumber;
+	String typeOfLoan; 
+	String propertyType; 
+	long amountDesired;
+	String nameOfBank;
+	String typeOfAccount;
+	long accountNumber;
+	long downPayment;
 	double totalDollars;
+
+	public loanTransaction (String n, String city, String state, String zip, String phoneNum, String loanType, String property, long amount, String bankName, String accountType, long accountNum, long payment)
+	{
+		name = n;
+		cityName = city;
+		stateName = state;
+		zipCode = zip;
+		phoneNumber = phoneNum;
+		typeOfLoan = loanType;
+		propertyType = property; 
+		amountDesired = amount;
+		nameOfBank = bankName;
+		typeOfAccount = accountType; 
+		accountNumber = accountNum;
+		downPayment = payment;
+	}
+
+	public String toString()
+	{
+		return name + " : " + "Type of Loan = " + typeOfLoan + " Amount Desired = " + amountDesired + " Down Payment = " + downPayment;
+	}
+	
+}
+ouble totalDollars;
 
 	public loanTransaction (String n, String loanType, String amount, String payment)
 	{
